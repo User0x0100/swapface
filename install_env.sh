413b5706478b8c19bf8e3436f6f9d18cfa790748
liaohaixun <liaohaixun@wumii.com>
2026-05-01 20:30:54 +0800

    fix: 更新tensorrt-cu12安装命令，修正拼写错误并添加升级选项

--- install_env.sh
+++ install_env.sh
@@ -3,4 +3,4 @@ pip install --extra-index-url https://pypi.nvidia.com --upgrade nvidia-dali-cuda
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu128
 conda install "ffmpeg"
 pip install torchcodec --index-url=https://download.pytorch.org/whl/cu128
-pip install tensorrt-cu12==10.9.0.34 huggingface_hub nscipy umpy onnx onnxscript onnxruntime-gpu opencv-python tensorboard tqdm segmentation-models-pytorch fvcore xformers torchinfo ninja
+pip install --upgrade tensorrt-cu12==10.9.0.34 huggingface_hub scipy numpy onnx onnxscript onnxruntime-gpu opencv-python tensorboard tqdm segmentation-models-pytorch fvcore xformers torchinfo ninja
