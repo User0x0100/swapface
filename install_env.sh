06644a97dacca6831688c93614bf777c263ead1d
liaohaixun <liaohaixun@wumii.com>
2026-05-01 20:45:56 +0800

    fix: 修正安装命令中的参数顺序以确保正确升级

--- install_env.sh
+++ install_env.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-pip install --extra-index-url https://pypi.nvidia.com --upgrade nvidia-dali-cuda120
-pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu128
+pip install --upgrade nvidia-dali-cuda120 --extra-index-url https://pypi.nvidia.com
+pip3 install --upgrade torch torchvision --index-url https://download.pytorch.org/whl/cu128
 conda install "ffmpeg"
 pip install torchcodec --index-url=https://download.pytorch.org/whl/cu128
 pip install --upgrade tensorrt-cu12==10.9.0.34 huggingface_hub scipy numpy onnx onnxscript onnxruntime-gpu opencv-python tensorboard tqdm segmentation-models-pytorch fvcore xformers torchinfo ninja
