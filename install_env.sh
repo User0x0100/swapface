a60d6b2b64ab28309df0cdd46f9a55bf3785d7a9
liaohaixun <1763767102@qq.com>
2026-08-31 15:13:51 +0800

    refactor: 更新安装脚本，移除bootstrap.sh并整合ffmpeg安装到install_env.sh

--- install_env.sh
+++ install_env.sh
@@ -1,6 +1,7 @@
-#/bin/bash
-pip install --upgrade nvidia-dali-cuda120 --extra-index-url https://pypi.nvidia.com
-pip3 install --upgrade torch torchvision --index-url https://download.pytorch.org/whl/cu128
-conda install "ffmpeg"
-pip install torchcodec --index-url=https://download.pytorch.org/whl/cu128
-pip install --upgrade tensorrt-cu12==10.9.0.34 huggingface_hub scipy numpy onnx onnxscript onnxruntime-gpu opencv-python tensorboard tqdm segmentation-models-pytorch fvcore xformers torchinfo ninja
+#!/usr/bin/env bash
+set -euo pipefail
+
+sudo apt-get update
+sudo apt-get install -y ffmpeg
+
+uv sync
